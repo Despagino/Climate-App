a0c3f6aa5c976f977b5f70f6f1112f417ce8a79a
Gino Tasis <gtasis84@gmail.com>
2022-02-28 18:11:14 -0500

    Arranged fil structure and added resources

---
+++ Climate/Controller/WeatherViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherViewController.swift
+//  Climate
+//
+//  Created by Gino Tasis on 2/28/22.
+//
+
+import Foundation
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        searchTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        <#code#>
+    }
+    
+}
