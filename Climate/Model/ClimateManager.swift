371d008d023248c7786700715b99fe8910d5b7a1
Gino Tasis <gtasis84@gmail.com>
2022-03-02 16:23:15 -0500

    implemented closure in task

--- Climate/Model/ClimateManager.swift
+++ Climate/Model/ClimateManager.swift
@@ -26,23 +26,22 @@ struct ClimateManager {
         let session = URLSession(configuration: .default)
             
         // 3. Give session a task
-        let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
             
         // 4. Start the task
         task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
-        }
-        
     }
-}
